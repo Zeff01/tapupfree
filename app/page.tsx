399c91040dfd3c510a08131de6477eeb1be051ce
zeff <jzeffsomera@gmail.com>
2024-06-07 16:31:22 +0800

    console

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import QRCode from "qrcode.react";
 
 export default function Home() {
   const router = useRouter();
@@ -9,6 +10,16 @@ export default function Home() {
   const navigateToCreate = () => {
     router.push("/create");
   };
+
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+  };
+  console.log("firebaseConfig:", firebaseConfig);
 
   return (
     <main className="flex min-h-screen bg-[#1E1E1E] text-white flex-col items-center pt-12 p-6 ">
@@ -32,6 +43,12 @@ export default function Home() {
       >
         Go to Users Table
       </button>
+      {/* <QRCode
+        value="https://bradwell-fe.vercel.app"
+        size={256} // Size of the QR code
+        level="H" // Error correction level: L, M, Q, H
+        includeMargin={true} // Include a margin around the QR code
+      /> */}
     </main>
   );
 }
